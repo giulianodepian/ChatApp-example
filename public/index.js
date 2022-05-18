9df1d7df633cc5336257be3f245fc99a05400742
Gonzalo Berteri <gonzaloberteri@gmail.com>
2022-05-17 21:15:26 -0300

    js y css separado de html

---
+++ public/index.js
@@ -0,0 +1,76 @@
+var socket = io();
+var form = document.getElementById('form');
+var nickForm = document.getElementById('nicknameform');
+var nickinput = document.getElementById('nickinput')
+var input = document.getElementById('input');
+var typing = document.getElementById('typing');
+
+input.addEventListener('change', function (e) {
+    e.preventDefault();
+    socket.emit('typing', input.value);
+})
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    if (input.value) {
+        socket.emit('chat message', input.value);
+        socket.emit('typing', '');
+        input.value = '';
+    }
+});
+nickForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    if (nickinput.value) {
+        socket.emit('user connected', nickinput.value);
+        window.nick = nickinput.value;
+        document.getElementById('popup').style.visibility = 'hidden';
+        document.getElementById('form').style.visibility = 'visible';
+        document.getElementById('messages').style.visibility = 'visible';
+    }
+})
+
+socket.on('user connected', (nick) => {
+    var item = document.createElement('li');
+    item.textContent = nick + ' Connected';
+    messages.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight);
+});
+
+socket.on('user disconnected', (nick) => {
+    var item = document.createElement('li');
+    item.textContent = nick + ' Disconnected';
+    messages.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight);
+});
+
+socket.on('typing', (writers) => {
+    var nickPos = writers.indexOf(window.nick);
+    if (nickPos != -1) {
+        writers.splice(nickPos, 1);
+    }
+    if (writers.length > 0) {
+        let text = '';
+        if (writers.length <= 3) {
+            writers.forEach(writer => {
+                text = text + ' ' + writer;
+            });
+        } else {
+            text = 'Various users'
+        };
+        if (writers.length == 1) {
+            text = text + ' is typing';
+        } else {
+            text = text + ' are typing';
+        }
+        typing.textContent = text;
+    } else {
+        typing.textContent = '';
+    }
+});
+
+socket.on('chat message', (msg, nick) => {
+    var item = document.createElement('li');
+    item.textContent = nick + ': ' + msg;
+    messages.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight);
+});
